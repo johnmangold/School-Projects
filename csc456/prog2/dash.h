934c26390273e9716d7a296dce1271b728dd57fa
John Mangold <john.mangold@mines.sdsmt.edu>
2015-03-02 08:03:50 -0700

    Finished program.  doesn't work. pretty much sucks.  not documented.

--- csc456/prog2/dash.h
+++ csc456/prog2/dash.h
@@ -2,9 +2,16 @@
 #define DASH_H
 
 #include <iostream>
-#include <string>
+#include <stdio.h> //gets
+#include <string.h> //strtok, strcmp
 #include <fstream>
-#include <sstream>
+#include <sys/wait.h> //wait
+#include <sys/time.h> //getrusage
+#include <sys/resource.h> //getrusage
+#include <stdlib.h> //get real path
 #include <vector>
+#include <string>
+#include <signal.h> //signal
+#include <fcntl.h> //open, creat, O_RDONLY
 
 #endif
--- csc456/prog2/functions.cpp
+++ csc456/prog2/functions.cpp
@@ -179,3 +179,272 @@ void systat()
 	cout << endl;
 	
 }//end systat function
+
+/*********** Functions for prog2 ******************************/
+void redir_in(char *args[])
+{
+	int fpt1, child_pid;
+	int wait_pid, status;
+	struct rusage usage;
+	struct timeval user_time, sys_time;
+
+	child_pid = fork();
+	if(child_pid != 0)
+	{
+		cout << "Child PID: " << child_pid << endl;
+	}
+
+	if(child_pid == 0)
+	{
+		if ((fpt1 = open(args[2], O_RDONLY)) == -1)
+		{
+			printf("Unable to open %s for reading.\n", args[2]);
+			exit(-1);
+		}
+		close(0);       // close child standard input
+		dup(fpt1);      // redirect the child input
+		close(fpt1);    // close unnecessary file descriptor
+
+		execl(args[0], args[0], 0);
+		perror("Exec failed: ");
+		exit(6);
+	}
+	else
+	{
+		wait_pid = wait(&status);
+		
+		getrusage(RUSAGE_CHILDREN, &usage);
+		user_time = usage.ru_utime;
+		sys_time = usage.ru_stime;
+		cout << "\nUser Time: " << user_time.tv_sec <<"."  					<<user_time.tv_usec 
+		     << "\nSystem Time: " << sys_time.tv_sec <<"." 
+		     << sys_time.tv_usec 
+		     << "\nPage Faults: " << usage.ru_majflt 
+		     << "\nSwaps: " << usage.ru_nswap << endl;
+	}
+}
+
+void redir_out(char *args[])
+{
+	int fpt2, child_pid;
+	int wait_pid, status;
+	struct rusage usage;
+	struct timeval user_time, sys_time;
+	
+	child_pid = fork();
+	if(child_pid != 0)
+	{
+		cout << "Child PID: " << child_pid << endl;
+	}
+
+	if(child_pid == 0)
+	{
+		if ((fpt2 = creat(args[2], 0644)) == -1)
+		{
+		printf("Unable to open %s for writing.\n", args[2]);
+		exit(-1);
+		}
+		close(1);       // close child standard output 
+		dup(fpt2);      // redirect the child output 
+		close(fpt2);    // close unnecessary file descriptor
+	
+		execl(args[0], args[0], 0);
+		perror("Exec failed: ");
+		exit(5);
+	}
+	else
+	{
+		wait_pid = wait(&status);
+		
+		getrusage(RUSAGE_CHILDREN, &usage);
+		user_time = usage.ru_utime;
+		sys_time = usage.ru_stime;
+		cout << "\nUser Time: " << user_time.tv_sec <<"."  					<<user_time.tv_usec 
+		     << "\nSystem Time: " << sys_time.tv_sec <<"." 
+		     << sys_time.tv_usec 
+		     << "\nPage Faults: " << usage.ru_majflt 
+		     << "\nSwaps: " << usage.ru_nswap << endl;
+	}
+}
+
+void change(char *args[])
+{
+	char temp[100];
+	int child_pid, wait_pid, status;
+	struct rusage usage;
+	struct timeval user_time, sys_time;
+	
+	strcpy(temp,"cd ");
+	strcat(temp,args[1]);
+
+	child_pid = fork();
+	if(child_pid != 0)
+	{
+		cout << "Child PID: " << child_pid << endl;
+	}
+	
+	if(child_pid == 0)
+	{
+		execl("/bin/sh", "-c", temp, (const char *)0);
+	}
+	else
+	{
+		wait_pid = wait(&status);
+	
+		getrusage(RUSAGE_CHILDREN, &usage);
+		user_time = usage.ru_utime;
+		sys_time = usage.ru_stime;
+		cout << "\nUser Time: " << user_time.tv_sec <<"."  					<<user_time.tv_usec 
+		     << "\nSystem Time: " << sys_time.tv_sec <<"." 
+		     << sys_time.tv_usec 
+		     << "\nPage Faults: " << usage.ru_majflt 
+		     << "\nSwaps: " << usage.ru_nswap << endl;
+	}
+}
+
+void pipe(char *args[])
+{
+	char temp1[100];
+	char temp2[100];
+	int fd_pipe[2];
+	int pid1;
+	int pid2;
+	int status;
+	int wait_pid;
+	struct rusage usage;
+	struct timeval user_time, sys_time;
+	
+	strcpy(temp1,"/bin/");
+	strcat(temp1,args[0]);
+	
+	strcpy(temp2,"/bin/");
+	strcat(temp2,args[2]);
+
+	pid1 = fork();
+	if(pid1 != 0);
+	{
+		cout << "Child 1 PID: " << pid1 << endl;
+	}
+	
+	if (pid1 == 0)
+	{
+		// child process executes here for input side of pipe
+
+		pipe(fd_pipe);           // create pipe
+
+		pid2 = fork();
+		if(pid2 != 0);
+		{
+			cout << "Child 2 PID: " << pid2 << endl;
+		}
+		
+		if (pid2 == 0)
+		{
+			// grandchild process executes here for output side of pipe
+			close(1);              // close standard output
+			dup(fd_pipe[1]);       // redirect the output
+			close(fd_pipe[0]);     // close unnecessary file descriptor
+			close(fd_pipe[1]);     // close unnecessary file descriptor
+			execl(temp2, args[2], 0);
+			printf("execl of /bin/cat failed\n");
+			exit(1);
+		}
+
+		// back to process for input side of pipe
+
+		close(0);              // close standard input
+		dup(fd_pipe[0]);       // redirect the input
+		close(fd_pipe[0]);     // close unnecessary file descriptor
+		close(fd_pipe[1]);     // close unnecessary file descriptor
+		execl(temp1, args[0], 0);
+		printf("execl of /bin/sort failed\n");
+		exit(1);
+	}
+	else
+	{
+		// parent process executes here
+		wait_pid = wait(&status);
+		
+		getrusage(RUSAGE_CHILDREN, &usage);
+		user_time = usage.ru_utime;
+		sys_time = usage.ru_stime;
+		cout << "\nUser Time: " << user_time.tv_sec <<"."  					<<user_time.tv_usec 
+		     << "\nSystem Time: " << sys_time.tv_sec <<"." 
+		     << sys_time.tv_usec 
+		     << "\nPage Faults: " << usage.ru_majflt 
+		     << "\nSwaps: " << usage.ru_nswap << endl;
+	}
+}
+
+void sig(char *args[])
+{
+	int status;
+	int wait_pid;
+	int child_pid;
+	struct rusage usage;
+	struct timeval user_time, sys_time;
+	
+	child_pid = fork();
+	if(child_pid != 0)
+	{
+		cout << "Child PID: " << child_pid << endl;
+	}
+	
+	if(child_pid == 0)
+	{
+		execl("/bin/kill", "kill", args[2], args[1], 0);
+		perror("Kill exec failed: ");
+		exit(10);
+	}
+	else
+	{
+		// parent process executes here
+		wait_pid = wait(&status);
+		
+		getrusage(RUSAGE_CHILDREN, &usage);
+		user_time = usage.ru_utime;
+		sys_time = usage.ru_stime;
+		cout << "\nUser Time: " << user_time.tv_sec <<"."  					<<user_time.tv_usec 
+		     << "\nSystem Time: " << sys_time.tv_sec <<"." 
+		     << sys_time.tv_usec 
+		     << "\nPage Faults: " << usage.ru_majflt 
+		     << "\nSwaps: " << usage.ru_nswap << endl;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
