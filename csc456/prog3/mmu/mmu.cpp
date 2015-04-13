b911f702d25cb82820033577efabb182aed09039
John Mangold <john.mangold@mines.sdsmt.edu>
2015-04-13 12:59:37 -0600

    created virtual and physical memory

--- csc456/prog3/mmu/mmu.cpp
+++ csc456/prog3/mmu/mmu.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main(int argc, char** argv)
 {
 	string num_swaps;
-	map<int, int> virtual_memory;
-	map<int, int> tlb;
-	map<int, int> page_table; 
-	map<int, int> physical_memory;
+	map<long long int, long long int> virtual_memory; //given 2^32 processes
+	map<int, int> tlb;  //must check that it doesn't contain more than 2^10
+	map<int, int> page_table; //page size 512kb so 2^13 for page table
+	map<int, int> physical_memory; //4GB(2^12)
 
 	cout << "Enter the number of desired page swaps: ";
 	getline(cin, num_swaps);
@@ -19,6 +19,25 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
+	//fill virutal_memory with addresses
+	for(long long int i = 0; i< pow(2,32);i++)
+	{
+		virtual_memory[i]=i;
+	}
+	
+	//fill physical_memory with random processes and addresses
+	//fix this by making it random for numbers below 2^32
+	for(int i = 0;i< pow(2,12);i++)
+	{
+		physical_memory[i] = i;
+	}
+	
+	//now begin swapping
+	//check tlb first, if not found, tlb_miss++, if found tlb_hit++
+	//check page table, find and place in physical memory
+	//if not found page_fault++, move on
+	//also time how long it takes to runt through all swaps
+	//get percentages of hits, misses, and page faults.
 
 	return 0;
 }
